c129cc3c98a10512123350f2c3d884b37dc591a6
keymanjacob <fghjohn@gmail.com>
2019-02-08 17:04:50 -0600

    adding new direction

--- Solutions.java
+++ Solutions.java
@@ -825,9 +825,40 @@ class Solution {
 Runtime: 6 ms, faster than 99.80% of Java online submissions for Lowest Common Ancestor of a Binary Tree.
 Memory Usage: 23.1 MB, less than 21.61% of Java online submissions for Lowest Common Ancestor of a Binary Tree.
 =======================================================================================================================
+#121. Best Time to Buy and Sell Stock
+Say you have an array for which the ith element is the price of a given stock on day i.
 
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 
+Note that you cannot sell a stock before you buy one.
 
+Example 1:
 
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+Example 2:
 
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+------------------------------------------------------
 
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++){
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+}
+
+Runtime: 1 ms, faster than 99.88% of Java online submissions for Best Time to Buy and Sell Stock.
+Memory Usage: 27.6 MB, less than 20.22% of Java online submissions for Best Time to Buy and Sell Stock.
+=====================================================================================================================================================

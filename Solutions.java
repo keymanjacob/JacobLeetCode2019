2b9e5c21e6b71d8747924da7db3537747e992216
keymanjacob <fghjohn@gmail.com>
2019-02-17 21:31:34 -0600

    adding new direction

--- Solutions.java
+++ Solutions.java
@@ -1402,3 +1402,51 @@ class Solution {
 Runtime: 0 ms, faster than 100.00% of Java online submissions for Remove Duplicates from Sorted List.
 Memory Usage: 37.7 MB, less than 100.00% of Java online submissions for Remove Duplicates from Sorted List.
 =================================================================================================================
+#844. Backspace String Compare
+Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Example 1:
+
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+Example 2:
+
+Input: S = "ab##", T = "c#d#"
+Output: true
+Explanation: Both S and T become "".
+Example 3:
+
+Input: S = "a##c", T = "#a#c"
+Output: true
+Explanation: Both S and T become "c".
+Example 4:
+
+Input: S = "a#c", T = "b"
+Output: false
+Explanation: S becomes "c" while T becomes "b".
+Note:
+
+1 <= S.length <= 200
+1 <= T.length <= 200
+S and T only contain lowercase letters and '#' characters.
+------------------------------
+class Solution {
+    public String build(String s) {
+            Stack<Character> ans = new Stack();
+            for (char c: s.toCharArray()) {
+                if (c!='#') {
+                    ans.push(c);
+                } else if (!ans.empty()) {
+                    ans.pop();
+                }
+            }
+            return String.valueOf(ans);
+    }
+    public boolean backspaceCompare(String S, String T) {
+        return build(S).equals(build(T));
+    }
+}
+Runtime: 7 ms, faster than 32.22% of Java online submissions for Backspace String Compare.
+Memory Usage: 37.2 MB, less than 100.00% of Java online submissions for Backspace String Compare.
+=================================================================================================================

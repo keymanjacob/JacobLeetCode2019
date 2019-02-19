58d870c28c2a5a01bd22b96e7ca5c9c57b59af2d
e45455 <guanhua.fan@fisglobal.com>
2019-02-19 11:30:43 -0600

    adding easy prob solutions

--- Solutions.java
+++ Solutions.java
@@ -1733,3 +1733,59 @@ class Solution {
 Runtime: 4 ms, faster than 98.92% of Java online submissions for Remove Element.
 Memory Usage: 37.6 MB, less than 100.00% of Java online submissions for Remove Element.
 =================================================================================================================
+#141. Linked List Cycle
+Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+
+Example 2:
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+
+Example 3:
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+--------------------------------
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow!=fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Linked List Cycle.
+Memory Usage: 38.3 MB, less than 100.00% of Java online submissions for Linked List Cycle.
+=================================================================================================================

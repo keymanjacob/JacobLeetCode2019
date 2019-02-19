d0da9e09320a405e5fdaffbf9bd478995a61dc62
e45455 <guanhua.fan@fisglobal.com>
2019-02-19 13:58:04 -0600

    adding easy prob solutions

--- Solutions.java
+++ Solutions.java
@@ -1789,3 +1789,57 @@ public class Solution {
 Runtime: 0 ms, faster than 100.00% of Java online submissions for Linked List Cycle.
 Memory Usage: 38.3 MB, less than 100.00% of Java online submissions for Linked List Cycle.
 =================================================================================================================
+#56. Merge Intervals
+Given a collection of intervals, merge all overlapping intervals.
+
+Example 1:
+
+Input: [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+Example 2:
+
+Input: [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+----------------------------
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    private class IntervalComparator implements Comparator<Interval> {
+        @Override
+        public int compare(Interval a, Interval b) {
+            return a.start < b.start ? -1 : a.start == b.start ? 0 : 1;
+        }
+    }
+
+    public List<Interval> merge(List<Interval> intervals) {
+        Collections.sort(intervals, new IntervalComparator());
+
+        LinkedList<Interval> merged = new LinkedList<Interval>();
+        for (Interval interval : intervals) {
+            // if the list of merged intervals is empty or if the current
+            // interval does not overlap with the previous, simply append it.
+            if (merged.isEmpty() || merged.getLast().end < interval.start) {
+                merged.add(interval);
+            }
+            // otherwise, there is overlap, so we merge the current and previous
+            // intervals.
+            else {
+                merged.getLast().end = Math.max(merged.getLast().end, interval.end);
+            }
+        }
+
+        return merged;
+    }
+}
+Runtime: 12 ms, faster than 92.42% of Java online submissions for Merge Intervals.
+Memory Usage: 41.1 MB, less than 100.00% of Java online submissions for Merge Intervals.
+=================================================================================================================

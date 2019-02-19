74d216203b75a0dc045285798945cee12ce35ebe
keymanjacob <fghjohn@gmail.com>
2019-02-18 23:20:52 -0600

    adding new direction

--- Solutions.java
+++ Solutions.java
@@ -1635,3 +1635,43 @@ Runtime: 14 ms, faster than 81.15% of Java online submissions for First Unique C
 Memory Usage: 36.8 MB, less than 100.00% of Java online submissions for First Unique Character in a String.
 =================================================================================================================
 #5. Longest Palindromic Substring
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+Example 2:
+
+Input: "cbbd"
+Output: "bb"
+-----------------------------
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+}
+Runtime: 10 ms, faster than 94.11% of Java online submissions for Longest Palindromic Substring.
+Memory Usage: 37.9 MB, less than 100.00% of Java online submissions for Longest Palindromic Substring.
+=================================================================================================================
